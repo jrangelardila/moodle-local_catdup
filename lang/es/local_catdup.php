1f4fc33daaefa519ce9eb814933d254bef6f32ff
Jhon Rangel <jrangelardila@gmail.com>
2025-01-05 20:03:09 -0500

    Create local_catdup.php, with the Spanish language.

---
+++ lang/es/local_catdup.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details.
+ *
+ * @package   local_catdup
+ * @copyright 2019 OpenApp By Yedidia Klein http://openapp.co.il
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Duplicado de Categoría';
+
+$string['origin'] = 'Categoría de Origen';
+$string['destination'] = 'Categoría de Destino';
+$string['taskinserted'] = 'Tu solicitud fue insertada en la cola, recibirás un correo electrónico cuando se complete.';
+$string['extension'] = 'Extensión de los nuevos cursos.';
+$string['oldextension'] = 'Extensión de los cursos antiguos.';
+$string['notempty'] = 'no está vacía.';
+$string['copiedto'] = 'copiado a';
+$string['accessdenied'] = 'Acceso Denegado';
+$string['privacy:metadata'] = 'El plugin de catdup no almacena ningún dato personal.';
+$string['selectdestination'] = 'Seleccionar Categoría de Destino';
+$string['selectorigin'] = 'Seleccionar Categoría de Origen';
